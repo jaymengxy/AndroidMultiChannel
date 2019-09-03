a4f0f5e879565ec96bf9582a9a9c164c9bd103fb
jaymengxy <jaymengsniper@gmail.com>
2019-09-03 17:53:03 +0800

    add variable arguments

--- multi-pack/multi-pack.sh
+++ multi-pack/multi-pack.sh
@@ -3,8 +3,13 @@
 APK_PATH=$1
 TEMP_PATH="../app/build/outputs/temp/"
 CHANNELS_OUT_PATH="../app/build/outputs/channels"
+# need replace
 ACCOUNT_360="360加固账号名"
 PASSWORD_360="360加固账号的密码"
+APK_KEYSTORE_PATH="../app/multi.jks"
+APK_KEYSTORE_PWD="mxymulti"
+APK_KEY_ALIAS="multiKey"
+APK_KEY_PWD="mxymulti"
 
 
 echo "########## jiagu start ##########"
@@ -23,7 +28,7 @@ ZIP_ALIGN_APK_PATH="${JIAGU_APK_PATH%.*}_aligned.apk"
 
 echo "########## sign ##########"
 SIGNED_APK_PATH="${ZIP_ALIGN_APK_PATH%.*}_signed.apk"
-../multi-pack/apksigner sign --ks ../app/multi.jks --ks-key-alias multiKey --ks-pass pass:mxymulti --key-pass pass:mxymulti --out ${SIGNED_APK_PATH} ${ZIP_ALIGN_APK_PATH}
+../multi-pack/apksigner sign --ks ${APK_KEYSTORE_PATH} --ks-key-alias ${APK_KEY_ALIAS} --ks-pass pass:${APK_KEYSTORE_PWD} --key-pass pass:${APK_KEY_PWD} --out ${SIGNED_APK_PATH} ${ZIP_ALIGN_APK_PATH}
 
 echo "########## channels ##########"
 rm -rf ${CHANNELS_OUT_PATH}
